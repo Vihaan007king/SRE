cab60e06aaecd7957142b10582fcf5d837d4271c
Vihaan007king <vihaansrivastava91@gmail.com>
2025-02-06 01:51:28 +0530

    I am adding this line in test2.txt fike through benach Feature

---
+++ code1.py
@@ -0,0 +1,14 @@
+
+class Hello:
+
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+    def print1(self):
+        print(f"Name{self.x}")
+
+
+obj1=Hello("Prakhar","Shruti")
+
+obj1.print1()
